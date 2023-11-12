d8f5981e078ef7cb300ba3720d0c9ddab1c9d297
Klajdi Hoxha <klainti95@gmail.com>
2023-11-12 23:02:58 +0100

    Changed project folder structure and created basic homepage structures

---
+++ scripts/script.js
@@ -0,0 +1,19 @@
+// Define functions to start different game modes
+function startLocalGame() {
+  window.location.href = 'pages/localPlay.html';
+}
+
+function startComputerGame() {
+  window.location.href = 'pages/computerPlay.html';
+}
+
+function startOnlineGame() {
+  window.location.href = 'pages/onlinePlay.html';
+}
+
+// Define functions to navigate between pages
+function goToHome() {
+  window.location.href = '../index.html';
+}
+
+// Add more functions as needed for specific page actions
